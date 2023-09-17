b960835922f95b1e4439cdb00b951c35143c7401
jhg9205 <93892965+jhg9205@users.noreply.github.com>
2023-09-17 23:28:45 +0900

    Delete src directory
    df

--- src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-/* ******************* 환경변수 ******************* */
-interface ImportMetaEnv {
-	readonly VITE_APP_IMGS: string
-	readonly VITE_APP_LOCAL_IMGS: string
-}
